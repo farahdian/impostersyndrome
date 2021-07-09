2fb1fa9b70e925715b2ed39fd4d603a549e3cc26
Farah Dianputri <farah.jachja@gmail.com>
2021-07-09 22:59:08 +0800

    Deleted hardcoded feed

--- src/Feed.js
+++ src/Feed.js
@@ -68,14 +68,7 @@ function Feed() {
             text={text}
             />))
             }
-
-        
-
-
-            <FeedCard user="New York Times" followers="1110" postTime="3 hrs" text="Facing grueling 72-hour work weeks and rising housing prices, many young people in China fear they will be the first generation not to do better than their parents. They are now defying the country’s long-held prosperity..." imgSrc="https://media-exp3.licdn.com/dms/image/sync/C5634AQFqpU0f3HVt-Q/ugc-proxy-shrink_800/0/1625742007899?e=1625842800&v=beta&t=bPOMzrRLR0FOzJhi3qlDfsVHK6oyRfu6ag6X-7-AIVo" title="Chinese Millenials are Chilling and Beijing isn't happy about that" subtitle="nytimes.com"/>
             </div>
-            <FeedCard user="That person from your high school" followers="50" postTime="2 mins" text="So proud to have achieved more than you. I'm not trying to brag just stating facts"></FeedCard>
-
            
         </div>
     )
